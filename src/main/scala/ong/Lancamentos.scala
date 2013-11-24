443a4f9617e07cf71aed628ef4bf433672de023f
Jonas Abreu <jonas@vidageek.net>
2013-11-24 18:11:59 -0200

    nao lembro

--- src/main/scala/ong/Lancamentos.scala
+++ src/main/scala/ong/Lancamentos.scala
@@ -1,13 +1,21 @@
 package ong
 
 import br.com.caelum.vraptor._
+import java.util.List
+import scala.collection.JavaConverters._
+import java.math.BigDecimal
 
 @Resource
 class Lancamentos(result : Result) {
 
   @Get(Array("/"))
-  def x() = {
-    println("asldkjaslkdjaskldjkj")
-    result.include("asd", "valor")
+  def novo() = {
+  }
+
+  @Post(Array("/novo"))
+  def postaNovo(lancamentosList : List[Lancamento]) = {
+    lancamentosList.asScala.foreach(println)
   }
 }
+
+case class Lancamento(produto : String, valor : BigDecimal)
---
+++ src/main/webapp/WEB-INF/jsp/lancamentos/novo.jsp
@@ -0,0 +1,16 @@
+<html>
+<head><title></title></head>
+<body>
+<form action="/novo" method="post">
+	<input name="lancamentosList[0].produto" />
+	<input name="lancamentosList[1].produto" />
+	<input name="lancamentosList[2].produto" />
+	<input name="lancamentosList[3].produto" />
+	<input name="lancamentosList[0].valor" />
+	<input name="lancamentosList[1].valor" />
+	<input name="lancamentosList[2].valor" />
+	<input name="lancamentosList[3].valor" />
+	<input type="submit" />
+</form> 
+</body>
+</html>
