33169cc84e83d7e2f3eb4b0a1db45ba5b403683e
Jonas Abreu <jonas@vidageek.net>
2014-04-13 16:51:20 -0300

    calculo do total suporta quantidade

--- src/main/webapp/js/ong.js
+++ src/main/webapp/js/ong.js
@@ -1,8 +1,8 @@
 function sumInputs(){
 	$("#total").attr("value",  
-	_.chain($("#compra .valor")).
+	_.chain($("#compra input.valor")).
 		foldl(function(soma, e){
-			return soma + (e.value * 1.0)
+			return soma + (e.value * e.previousElementSibling.previousElementSibling.value)
 		},0).value().toFixed(2))
 }
 
