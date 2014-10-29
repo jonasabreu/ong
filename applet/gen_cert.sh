0fc153eea5e93c452031ad7229920c1c2310c686
Jonas Abreu <jonas@vidageek.net>
2014-10-29 20:08:38 -0200

    contornando bug que impede geração de chaves com curvas elípticas

--- applet/gen_cert.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-openssl genrsa -out my_key.key 2048
-openssl req -new -key my_key.key -out my_request.csr
-openssl x509 -req -days 3650 -in my_request.csr -signkey my_key.key -out my_cert.crt
-openssl pkcs12 -keypbe PBE-SHA1-3DES -certpbe PBE-SHA1-3DES -export -in my_cert.crt -inkey my_key.key -out cert.pfx -name "my-name"
-
-rm -f my_key.key my_request.csr my_cert.crt 
--- applet/gen_keystore.sh
+++ applet/gen_keystore.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -f keystore.jks
-keytool -storepass "123456" -keypass "123456" -genkeypair -alias selfsignedtest -keyalg EC -keysize 571 -keystore keystore.jks
+keytool -storepass "123456" -keypass "123456" -genkeypair -alias selfsignedtest -keyalg RSA -keysize 2048 -keystore keystore.jks
 
 echo "Chave gerada. Verificando:"
 
