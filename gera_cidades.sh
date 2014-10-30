4d736060b3bacc722fc2c2c23f62f0b3dd029d68
Jonas Abreu <jonas@vidageek.net>
2014-10-30 20:58:46 -0200

    gerando options de cidades

--- gera_cidades.sh
+++ gera_cidades.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cat dtb_2013.csv | awk -F ";" '{print $1$7":\""$8"\","}' | sort | uniq
+cat dtb_2013.csv | grep -v "^Uf" | awk -F ";" '{print "<option value=\""$1$7"\">"$8"</option>"}' | sort | uniq
